a9993c778a1895b9aef3cf4fb21b8f74186f4cd2
paarthd00 <paarthcode@gmail.com>
2024-03-25 04:33:53 -0700

    Remove Junk

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/', [StripeController::class, 'index']);
 Route::post('/checkout', [StripeController::class, 'checkout'])->name('checkout');
 Route::get('/success', [StripeController::class, 'success'])->name('success');
 Route::get('/cancel', [StripeController::class, 'cancel'])->name('cancel');
