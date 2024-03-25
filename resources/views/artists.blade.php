d3198abd140c2e81fce5d1d9dddb5cffdd89db8a
paarthd00 <paarthcode@gmail.com>
2024-03-25 06:10:31 -0700

    Fix Artists layout

--- resources/views/artists.blade.php
+++ resources/views/artists.blade.php
@@ -1,13 +1,28 @@
-<!-- loop over artist -->
-@foreach($artists as $artist)
-<div class="card">
-    <img src="{{ asset('storage/'.$artist->image) }}" alt="{{ $artist->name }}">
-    <div class="card-body">
-        <h2>{{ $artist->name }}</h2>
-        <p>{{ $artist->bio }}</p>
-        <p>{{ $artist->price }}</p>
-        <a href="mailto:{{ $artist->email }}">{{ $artist->email }}</a>
-        <a href="tel:{{ $artist->phone }}">{{ $artist->phone }}</a>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Artists') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12 flex flex-wrap">
+        <!-- loop over artist -->
+        @foreach($artists as $artist)
+        <div class="card p-6">
+            <img class="text-white" src="{{ asset('storage/'.$artist->image) }}" alt="{{ $artist->name }}">
+            <div class="card-body">
+                <h2 class="text-white">{{ $artist->name }}</h2>
+                <p class="text-white">{{ $artist->bio }}</p>
+                <p class="text-white">{{ $artist->price }}</p>
+                <a class="text-white" href="mailto:{{ $artist->email }}">{{ $artist->email }}</a>
+                <br>
+                <a class="text-white" href="tel:{{ $artist->phone }}">{{ $artist->phone }}</a>
+            </div>
+        </div>
+        @endforeach
     </div>
-</div>
-@endforeach
+</x-app-layout>
+
+
+
+
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,6 +19,11 @@
                         {{ __('Add Artists') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('artists')" :active="request()->routeIs('artists')">
+                        {{ __('Artists') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
